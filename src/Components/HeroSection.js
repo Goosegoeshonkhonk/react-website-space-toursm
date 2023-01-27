7618a2fafa1218ad63959b1ffec5bfe373017895
Goosegoeshonkhonk <nguyenminhngoc1001110@gmail.com>
2023-01-27 16:18:54 +0000

    Update 5 files

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -1,30 +1,34 @@
-import React from 'react'
-import '../App.css';
-import {Button} from './Button';
-import './HeroSection.css';
-import bg_video from '/Users/mac/Documents/Workspace/react-website-space-toursm/src/Video/Space.mp4'
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./HeroSection.css";
+import {bg_video} from "././Video/Space.mp4";
 
 function HeroSection() {
   return (
-    <div className='hero-container'>
-        <video className='videoTag' autoPlay loop muted>
-            <source src={bg_video} type='video/mp4' />
-        </video>
-        <h1>SPACE TOURISM</h1>
-        <p>Spectacular adventures just one click away!</p>
-        <div className='hero-btns'>
-            <Button className='btns' buttonStyle='btn--outline'
-            buttonSize='btn--large'
-            >
-                MORE INFOR
-            </Button>
+    <div className="hero-container">
+      <video className="videoTag" autoPlay loop muted>
+        <source src={bg_video} type="video/mp4" />
+      </video>
+      <h1>SPACE TOURISM</h1>
+      <p>Spectacular adventures just one click away!</p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          MORE INFOR
+        </Button>
 
-            <Button className='btns' buttonStyle='btn--primary'
-            buttonSize='btn--large'
-            >
-                REGISTER NOW! 
-            </Button>
-        </div>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          REGISTER NOW!
+        </Button>
+      </div>
     </div>
   );
 }
